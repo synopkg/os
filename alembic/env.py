323dd8517d6336c9fad18e5972ce13e214b18f88
Peter Palfrader <peter@palfrader.org>
2017-02-10 20:06:19 +0100

    alembic: load our model

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,10 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+import os, sys
+sys.path.append(os.getcwd())
+import dmt.DB
+target_metadata = dmt.DB.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
