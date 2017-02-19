f32763b10bf84f48087fd8a1b26fc2cc4764d708
Peter Palfrader <peter@palfrader.org>
2017-02-19 21:27:05 +0100

    move get_ftpmaster_trace to helpers

--- dmt/helpers.py
+++ dmt/helpers.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 import urllib
-
+from sqlalchemy import desc
 
 if __name__ == '__main__' and __package__ is None:
+
     from pathlib import Path
     top = Path(__file__).resolve().parents[1]
     sys.path.append(str(top))
     import dmt.helpers
     __package__ = 'dmt.helpers'
+
+import dmt.db as db
+
+
+FTPMASTER = "ftp-master.debian.org"
 
 def get_tracedir(site):
     baseurl = urllib.parse.urljoin("http://" + site['name'], site['http_path'])
@@ -16,3 +22,12 @@ def get_tracedir(site):
     tracedir = urllib.parse.urljoin(baseurl, 'project/trace/')
     return tracedir
 
+def get_ftpmaster_trace(session):
+    ftpmastertraceq = session.query(db.Site, db.Mastertrace).filter_by(name = FTPMASTER). \
+                      join(db.Mastertrace).filter(db.Mastertrace.trace_timestamp.isnot(None)). \
+                      join(db.Checkrun). \
+                      order_by(desc(db.Checkrun.timestamp)).first()
+    if ftpmastertraceq is not None:
+        return ftpmastertraceq[1].trace_timestamp
+    else:
+        return None
