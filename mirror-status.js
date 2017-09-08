7c323273cbba7427b4f4dd8b9f515696bd4999f8
Bastian Blank <waldi@debian.org>
2017-09-08 21:45:28 +0000

    Sort site name on request

--- mirror-status.js
+++ mirror-status.js
@@ -1,6 +1,13 @@
 $(function() {
   // call the tablesorter plugin
   $("#results").tablesorter({
+    textSorter: {
+      0: function(a, b, direction, column, table){
+        var list_a = a.split('.'); list_a.reverse(); a = list_a.join('.');
+        var list_b = b.split('.'); list_b.reverse(); b = list_b.join('.');
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+      },
+    },
     widgets : [ "zebra", "filter" ],
     widgetOptions: {
       filter_useParsedData: false,
